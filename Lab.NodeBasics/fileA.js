a7090d8613a95f11e56d84d0c23d377ebe2dbf44
shawns21 <shawnsinanan21@gmail.com>
2023-07-10 13:53:14 -0400

    added node basics

---
+++ Lab.NodeBasics/fileA.js
@@ -0,0 +1,8 @@
+console.log(require('./file1')); // note the relative file path
+console.log(require('./file2')); // note the relative file path
+console.log(require('./file3')); // note the relative file path
+
+
+
+
+
